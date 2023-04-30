13ad6044c7065e8cc56b874ef66721d3a134cfe2
Phongphiphat Senta <MiniMarkChanel @gmail.com>
2023-05-01 04:14:49 +0700

    (Update) Home Page List

---
+++ go_together/lib/menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MenuPage extends StatefulWidget {
+  @override
+  _MenuState createState() => _MenuState();
+}
+
+class _MenuState extends State<MenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Menu"),
+    );
+  }
+}
---
+++ go_together/lib/notification.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotificationPage extends StatefulWidget {
+  @override
+  _NotificationState createState() => _NotificationState();
+}
+
+class _NotificationState extends State<NotificationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Notification"),
+    );
+  }
+}
