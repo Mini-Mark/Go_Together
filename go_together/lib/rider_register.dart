940aa765c60baf9c5cce13d7024b1ab540cc6487
Phongphiphat Senta <MiniMarkChanel @gmail.com>
2023-05-02 11:51:39 +0700

    (Update) Rider Register

--- go_together/lib/rider_register.dart
+++ go_together/lib/rider_register.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 
 class RiderRegisterPage extends StatefulWidget {
   @override
@@ -8,193 +6,296 @@ class RiderRegisterPage extends StatefulWidget {
 }
 
 class _RiderRegisterState extends State<RiderRegisterPage> {
-  File? _image;
-
-  Future getImage() async {
-    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (image == null) return;
-
-    final imageTemporary = File(image.path);
-
-    setState(() {
-      this._image = imageTemporary;
-    });
-  }
+  bool driving_toggle = false;
+  bool carImage_toggle = false;
+  bool license_toggle = false;
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      padding: EdgeInsets.only(bottom: 20, top: 20),
-      child: Expanded(
-        child: Column(
-          children: [
-            TextFormField(
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter your password';
-                }
-                return null;
-              },
-              obscureText: true,
-              decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(),
-                  border: OutlineInputBorder(),
-                  label: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(right: 8.0),
-                        child: Icon(
-                          Icons.drive_eta,
-                          size: 20,
-                          color: Colors.black87,
-                        ),
+      child: Column(
+        children: [
+          TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+            obscureText: true,
+            decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(),
+                border: OutlineInputBorder(),
+                label: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 8.0),
+                      child: Icon(
+                        Icons.drive_eta,
+                        size: 20,
+                        color: Colors.black87,
                       ),
-                      Text(
-                        "Brand",
-                        style: TextStyle(
-                          color: Colors.black87,
-                        ),
+                    ),
+                    Text(
+                      "Brand",
+                      style: TextStyle(
+                        color: Colors.black87,
                       ),
-                    ],
-                  )),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            TextFormField(
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter your password';
-                }
-                return null;
-              },
-              obscureText: true,
-              decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(),
-                  border: OutlineInputBorder(),
-                  label: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(right: 8.0),
-                        child: Icon(
-                          Icons.text_fields_rounded,
-                          size: 20,
-                          color: Colors.black87,
-                        ),
+                    ),
+                  ],
+                )),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+            obscureText: true,
+            decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(),
+                border: OutlineInputBorder(),
+                label: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 8.0),
+                      child: Icon(
+                        Icons.text_fields_rounded,
+                        size: 20,
+                        color: Colors.black87,
                       ),
-                      Text(
-                        "Model",
-                        style: TextStyle(
-                          color: Colors.black87,
-                        ),
+                    ),
+                    Text(
+                      "Model",
+                      style: TextStyle(
+                        color: Colors.black87,
                       ),
-                    ],
-                  )),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            TextFormField(
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter your password';
-                }
-                return null;
-              },
-              obscureText: true,
-              decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(),
-                  border: OutlineInputBorder(),
-                  label: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(right: 8.0),
-                        child: Icon(
-                          Icons.color_lens,
-                          size: 20,
-                          color: Colors.black87,
-                        ),
+                    ),
+                  ],
+                )),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+            obscureText: true,
+            decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(),
+                border: OutlineInputBorder(),
+                label: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 8.0),
+                      child: Icon(
+                        Icons.color_lens,
+                        size: 20,
+                        color: Colors.black87,
                       ),
-                      Text(
-                        "Color",
-                        style: TextStyle(
-                          color: Colors.black87,
-                        ),
+                    ),
+                    Text(
+                      "Color",
+                      style: TextStyle(
+                        color: Colors.black87,
                       ),
-                    ],
-                  )),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            TextFormField(
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter your password';
-                }
-                return null;
-              },
-              obscureText: true,
-              decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(),
-                  border: OutlineInputBorder(),
-                  label: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(right: 8.0),
-                        child: Icon(
-                          Icons.my_library_books_outlined,
-                          size: 20,
-                          color: Colors.black87,
-                        ),
+                    ),
+                  ],
+                )),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+            obscureText: true,
+            decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(),
+                border: OutlineInputBorder(),
+                label: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 8.0),
+                      child: Icon(
+                        Icons.my_library_books_outlined,
+                        size: 20,
+                        color: Colors.black87,
                       ),
-                      Text(
-                        "License No.",
-                        style: TextStyle(
-                          color: Colors.black87,
-                        ),
+                    ),
+                    Text(
+                      "License No.",
+                      style: TextStyle(
+                        color: Colors.black87,
                       ),
-                    ],
-                  )),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            GestureDetector(
-              onTap: () {
-                getImage();
-              },
-              child: Image.network(
-                'https://i.redd.it/lo49om2517pa1.png',
-                height: 250,
-                width: double.infinity,
-                fit: BoxFit.cover,
+                    ),
+                  ],
+                )),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          (driving_toggle
+              ? Image.asset(
+                  'assets/car_driving.jpg',
+                  height: 250,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      driving_toggle = !driving_toggle;
+                    });
+                  },
+                  child: Container(
+                      height: 250,
+                      width: double.infinity,
+                      color: Colors.black87,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.image_search_rounded,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "Choose Driving Image",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      )),
+                )),
+          SizedBox(
+            height: 15,
+          ),
+          (carImage_toggle
+              ? Image.asset(
+                  'assets/car_image.jpg',
+                  height: 250,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      carImage_toggle = !carImage_toggle;
+                    });
+                  },
+                  child: Container(
+                      height: 250,
+                      width: double.infinity,
+                      color: Colors.black87,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.image_search_rounded,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "Choose Car image",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      )),
+                )),
+          SizedBox(
+            height: 15,
+          ),
+          (license_toggle
+              ? Image.asset(
+                  'assets/car_license.jpg',
+                  height: 250,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      license_toggle = !license_toggle;
+                    });
+                  },
+                  child: Container(
+                      height: 250,
+                      width: double.infinity,
+                      color: Colors.black87,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.image_search_rounded,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "Choose License Image",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      )),
+                )),
+          SizedBox(
+            height: 15,
+          ),
+          Divider(
+            height: 1,
+            color: Colors.black45,
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // _submitForm();
+            },
+            child: Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: 40,
+              child: Text(
+                "Register",
+                style: TextStyle(fontSize: 18, color: Colors.black87),
               ),
             ),
-            SizedBox(
-              height: 15,
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Color(0xFF4EFF55)),
             ),
-            Image.network(
-              'https://i.redd.it/lo49om2517pa1.png',
-              height: 250,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Image.network(
-              'https://i.redd.it/lo49om2517pa1.png',
-              height: 250,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            )
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+        ],
       ),
     );
   }
